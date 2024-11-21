0a7093342c47d23f2f50f98ebae78c31662f3df1
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2024-11-21 17:48:50 +0200

    Fixes

--- src/Attributes/ResultType.php
+++ src/Attributes/ResultType.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace TTBooking\UniQuery\Attributes;
 
 use Attribute;
-use TTBooking\UniQuery\ResultInterface;
 
-/**
- * @template TResult of ResultInterface
- */
 #[Attribute(Attribute::TARGET_CLASS)]
 class ResultType
 {
     /**
-     * @param class-string<TResult> $type
+     * @param class-string $type
      */
     public function __construct(public string $type) {}
 }
